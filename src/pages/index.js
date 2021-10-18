4d0ed47d659553c3fec890230d174ce09586bc26
charishma-a <87021852+charishma-a@users.noreply.github.com>
2021-10-18 12:08:05 -0400

    website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import About from '../components/About/About'
-import Footer from '../components/Footer/Footer'
 import HeroSection from '../components/HeroSection'
 
 const Home = () => {
